f3d2d934dbcd0fa335e759445c6b6bf742747d42
Juan Santamaria <juansantamariabareno@gmail.com>
2021-12-21 12:24:23 -0500

    Retomando clase de List comprehensions

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,7 +1,12 @@
 def run():
-    cuadrados = []
-    for x in range(1,101):
-        cuadrados.append(x**2)
+    # cuadrados = []
+    # for x in range(1,101):
+    #     if x % 3 != 0:
+    #         cuadrados.append(x**2)
+    # print(cuadrados)
+
+    # Usando list comprehensions
+    cuadrados = [x**2 for x in range(1, 101) if x % 3 != 0]
     print(cuadrados)
     
 # Creo mi entrypoint
