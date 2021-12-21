c32af46d6a09668df1c9b7d9510b2e2179137095
Juan Santamaria <juansantamariabareno@gmail.com>
2021-12-21 16:37:54 -0500

    Filtrando datos

---
+++ filtrando_datos.py
@@ -0,0 +1,93 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    # Comprehensions solutions
+    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+
+    # Para sacar los mayores de cierta edad usaremos las High Order Functions
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    # La linea anterior si la imprimimos arroja todo el diccionario, pero yo quiero solo el nombre
+    # Para eso usamos una combinación de Hight Order Functions
+    # En este caso sobreescribimos la variable aplicando map sobre cada worker
+    # extrayendo el nombre
+    adults = list(map(lambda worker: worker["name"], adults))
+    # Ahora creamos una variable old para tener una lista de diccionarios y extraer los mayores de 18 con un 
+    # true or false
+    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+    for worker in old_people:
+        print(worker)
+
+if __name__ == '__main__':
+    run()
